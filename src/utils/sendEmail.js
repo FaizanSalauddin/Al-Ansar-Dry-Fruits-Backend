04e1211accd333a18d060fb2d474874ffd0c80df
Faizan Salauddin <salauddinfaizan@gmail.com>
2026-02-14 22:38:55 +0530

    updated email system using resend

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,22 +1,40 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async ({ to, subject, html }) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: `"Al-Ansar Stores" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: `"Al-Ansar Stores" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  };
-
-  await transporter.sendMail(mailOptions);
+  try {
+    await resend.emails.send({
+      from: "Al-Ansar Stores <onboarding@resend.dev>",
+      to,
+      subject,
+      html,
+    });
+  } catch (error) {
+    console.error("Email error:", error);
+  }
 };
 
 export default sendEmail;
