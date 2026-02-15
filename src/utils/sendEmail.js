5b22d393beece6f125b68bb51b4b4cd2358b10f5
Faizan Salauddin <salauddinfaizan@gmail.com>
2026-02-15 16:17:45 +0530

    added domain

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,41 +1,20 @@
-// import nodemailer from "nodemailer";
-
-// const sendEmail = async ({ to, subject, html }) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: `"Al-Ansar Stores" <${process.env.EMAIL_USER}>`,
-//     to,
-//     subject,
-//     html,
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// };
-
-// export default sendEmail;
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    await resend.emails.send({
-      from: "Al-Ansar Stores <onboarding@resend.dev>",
+    const data = await resend.emails.send({
+      from: "Al-Ansar Stores <noreply@al-ansar.webxprojects.com>",
       to,
       subject,
       html,
     });
+
+    console.log("Email sent:", data);
   } catch (error) {
     console.error("Email error:", error);
   }
 };
 
 export default sendEmail;
-
